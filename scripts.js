67a6617e7de8a41b0207dc32252575d1002a76e0
Hermes <hermeschigoziri@gmail.com>
2022-01-03 23:23:49 +0100

    Make layout for project

--- scripts.js
+++ scripts.js
@@ -1,14 +1,33 @@
+let myLib = [];
+
 function Book(title, author, pages, isRead)
 {
     this.title = title;
     this.author = author;
     this.pages = +pages;
-    this.read = isRead === true ? "has been read" : "not read yet";
+    this.read = isRead.toUpperCase() === "Y" ? "has been read" : "not read yet";
 }
 
 Book.prototype.info = function() {
     return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
 }
 
-let Test = new Book('Fablehaven', 'Brandon Mull', 400, true);
-console.log(Test.info());
+function addBookToLibrary()
+{
+    let bkTitle = prompt('Title of the Book?');
+    let bkAuthor = prompt('Author of the Book?');
+    
+    let bkPages = undefined;
+    while(+bkPages == NaN || +bkPages < 0)
+    {
+        bkPages = prompt('Number of pages in the book?');
+    }
+    
+    let bkRead = '';
+    while(bkRead.toUpperCase() != 'Y' && bkRead.toUpperCase() != 'N')
+    {
+        bkRead = String(prompt('Has the book been read? (Y/N)'));
+    }
+
+    myLib.push(new Book(bkTitle, bkAuthor, bkPages, bkRead));
+}
